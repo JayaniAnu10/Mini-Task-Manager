278766330bbbafabb05fa5a70469e6d05ae44151
Jayani Anuththara <jayanianuththara10@gmail.com>
2026-03-10 11:34:17 +0530

    Add flyway migration and the initial migration file.

---
+++ backend/src/main/resources/db/migration/V1__initial_migration.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       email VARCHAR(150) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       role VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE tasks (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(255) NOT NULL,
+                       description TEXT,
+                       status VARCHAR(20),
+                       priority VARCHAR(20),
+                       due_date DATE,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                       user_id BIGINT,
+                       CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO users (email, password, role)
+VALUES ('admin@gmail.com', '$2a$12$xiJr3Pv0vsSf79USrIE9oeFHYdUyG7jIOr7DyZPFDuN/bSvMkNAkC', 'ADMIN');
